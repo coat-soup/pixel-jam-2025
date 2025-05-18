62ca3c94eab0c62ca7da91572738e7efa49cd107
Absurd24 <160165230+Absurd24@users.noreply.github.com>
2025-05-17 22:37:24 -0400

    added player anims and blood particle

--- player/scripts/player_movement.gd
+++ player/scripts/player_movement.gd
@@ -4,11 +4,24 @@ extends Node
 
 @export var speed := 400.0
 
+@onready var playerSprite = $"../AnimatedSprite2D"
+
 
 func get_input() -> Vector2:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	return input_dir
+	
+	
 
 func _physics_process(delta: float) -> void:
 	player.velocity = get_input() * speed
 	player.move_and_slide()
+	
+	if Input.is_action_pressed("left"):
+		playerSprite.play("PLeft")
+	if Input.is_action_pressed("right"):
+		playerSprite.play("PRight")
+	if Input.is_action_pressed("up"):
+		playerSprite.play("PUp")
+	if Input.is_action_pressed("down"):
+		playerSprite.play("PDown")
